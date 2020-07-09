2315904c42dcaa2f50cfda3fa8e8d79da3c31e75
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-09 07:17:52 -0700

    Added all necessary prompts. Added writeFile to create READme.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# READme_Generator
+{
+	"title": "Best title",
+	"description": "Best Descript",
+	"tableOfContents": "Table Table",
+	"install": "Download",
+	"usage": "use reset",
+	"license": "MIT",
+	"contributors": "Me you and dupree",
+	"tests": "You just do it yo",
+	"questions": "yu"
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 
-inquirer.prompt([{
+inquirer.prompt(
+    [{
         // Title
         type: "input",
         name: "title",
@@ -25,57 +26,64 @@ inquirer.prompt([{
         message: "How do you install this app?",
         name: "install"
     },
-
-    //   type: "list",
-    //   message: "What is your preferred method of communication?",
-    //   name: "contact",
-    //   choices: [
-    //     "email",
-    //     "phone",
-    //     "telekinesis"
-    //   ]
-
-
-    // Installation
-    // Usage
-    // License
-    // Contributing
-    // Tests
-    // Questions
-
-
+    {
+        // Usage
+        type: "input",
+        message: "Please describe the usage of the app:",
+        name: "usage"
+    },
+    {
+      type: "list",
+      message: "What license did you use?",
+      name: "license",
+      choices: [
+        "MIT",
+        "GPL v3",
+        "AGPL"
+      ]
+    },
+    {
+        // Contributer
+        type: "input",
+        message: "List all contributors:",
+        name: "contributors"
+    },
+    {
+        // Tests
+        type: "input",
+        message: "Please describe the usage of the app:",
+        name: "tests"
+    },
+    {
+        // Questions
+        type: "input",
+        message: "Are there any questions that remain about this project?",
+        name: "questions"
+    },
 
     // The generated README includes 1 badge that's specific to the repository.
+
+
+
+// Creates object from user inputs 
 ]).then(function (data) {
 
     console.log(data);
-
-    // var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-    // fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-    //   if (err) {
-    //     return console.log(err);
-    //   }
-
-    console.log("Success!");
-
-    // });
 });
 
 
+// Takes object and puts it in READme
+var filename = "README.md"
+    console.log(filename);
 
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
 
-//   Title
-// Description
-// Table of Contents
-// Installation
-// Usage
-// License
-// Contributing
-// Tests
-// Questions
+      if (err) {
+        return console.log(err);
+      }
 
+    console.log("Success!");
 
+    });
 
 // The generated README includes 1 badge that's specific to the repository.
